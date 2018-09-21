fcda93a10f37af21a974d0ca9c10f9206a33330d
andimalfresh <w.andrew.pedersen@gmail.com>
2018-09-21 12:41:45 -0600

    styled CSS

--- js/events.js
+++ js/events.js
@@ -1,8 +1,37 @@
-var submitButton = document.querySelector('input[type=submit]')
+// var submitButton = document.querySelector('total')
+// submitButton.addEventListener('click', submitForm)
+
+// function submitForm(event) {
+//     var totalInput = document.getElementById('total')
+//     var total = totalInput.value
+//     console.log(total)
+// }
+
+
+//     function Calculate()
+//     {
+//         var TotalProductionTime = document.getElementById("TotalProductionTime").value;
+//         var TotalProductionTimeInMinutes = TotalProductionTime * 60;
+//         var Breaks = document.getElementById("Breaks").value;
+//         var Malfunctions = document.getElementById("Malfunctions").value;
+//         var TheoreticalProductionTime = TotalProductionTimeInMinutes - Breaks - Malfunctions;       
+
+//         document.getElementById("test").innerHTML = TheoreticalProductionTime;
+//     }
+// </script>   
+
+// </body>
+// </html>
+
+var submitButton = document.querySelector('button')
 submitButton.addEventListener('click', submitForm)
 
 function submitForm(event) {
-    var totalInput = document.getElementById('total')
-    var total = totalInput.value
-    console.log(total)
+
+    var ticket = document.getElementById('total').value
+
+    function calculateTip(event) {
+        newTip = number = number * .2
+        return newTip
+    }
 }
