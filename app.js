c8dec61f419d530f394011731b1b1febf0505ac3
andimalfresh <w.andrew.pedersen@gmail.com>
2018-09-26 11:52:17 -0600

    Story #2 completed

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
 
-
-for (var i = 0; i < 99; i++) {
-    console.log('^ _ ^')
-}
-
 var buttonPush = document.querySelector('.btn')
 var grabTotal = document.querySelector('.total')
 
-
 buttonPush.addEventListener('click', function (event) {
     event.preventDefault()
-    var mealCost = document.querySelector('.total').value
+    var message = 'Your Total for today is: $'
+    var mealCost = parseInt(document.querySelector('.total').value)
     var mealPlusTip = mealCost + (mealCost * .2)
-    document.querySelector('.return').innerText += mealPlusTip
+    mealPlusTip = mealPlusTip.toFixed(2)
+    document.querySelector('.return').innerText += message + mealPlusTip
 })
 
-
-
-
-
-
-
-
-
-
-
-// var costInput = document.querySelector('.total').value
-// var buttonPress = document.querySelector('.btn')
-// buttonPress.addEventListener('click', function (event) {
-//     document.appendChild('paragrap')
-// })
