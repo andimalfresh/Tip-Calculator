43d13fa68b4a784fae16a93a9587714b2521231f
andimalfresh <w.andrew.pedersen@gmail.com>
2018-10-01 14:10:06 -0600

    Fixed Project - Simplified Code and got rid of the 'DRY' Css

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 
 var buttonPush = document.querySelector('.btn')
 var grabTotal = document.querySelector('.total')
+var message = 'The amount you should tip is: $'
+
 
 buttonPush.addEventListener('click', function (event) {
     event.preventDefault()
-    var message = 'Your Total for today is: $'
-    var mealCost = parseInt(document.querySelector('.total').value)
-    var mealPlusTip = mealCost + (mealCost * .2)
-    mealPlusTip = mealPlusTip.toFixed(2)
-    document.querySelector('.return').innerText += message + mealPlusTip
+    var pullTotal = parseInt(document.querySelector('.total').value)
+    var tipAmount = (pullTotal * .2).toFixed(2)
+    document.querySelector('.return').innerText += message + tipAmount
 })
 
